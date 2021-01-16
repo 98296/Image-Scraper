bd231d686501246cd382837a74711500b5422a6a
travis <travis.hend@gmail.com>
2021-01-16 14:12:43 -0800

    This is working nicely so far, was able to make a map with titles and links, more to come!

--- main.go
+++ main.go
@@ -20,31 +20,55 @@ func fetchHTML(url string) io.ReadCloser {
 	return resp.Body
 }
 
-func tokenize(body io.ReadCloser) {
+func tokenize(body io.ReadCloser) map[string]string {
+	retval := make(map[string]string)
 	z := html.NewTokenizer(body)
 	for {
 		tt := z.Next()
 		if tt == html.ErrorToken {
-			return
+			return retval
 		}
 
 		if tt == html.StartTagToken { // I'm looking for first occurence of <ol>
 			token := z.Token()
 			if token.Data == "ol" {
-				tt = z.Next() // Text, most likely '\n'.
-				tt = z.Next() // Start tag, <li>
-				tt = z.Next() // Start tag <a>
-				//tt = z.Next() // Is <a>THIS</a>
-				token = z.Token()
-				fmt.Println(token.Data)
-				ta := token.Attr
-				fmt.Println(len(ta))
-				fmt.Println(ta)
-				if len(ta) > 0 {
-					fmt.Println("EXISTS")
-					//fmt.Println(tn[0])
+				for { // Inside <ol></ol>
+					// Find me all <a>
+					for tt != html.StartTagToken || token.Data != "a" {
+						tt = z.Next()
+						token = z.Token()
+						if tt == html.EndTagToken && token.Data == "ol" {
+							fmt.Println(token.Data)
+							return retval
+						}
+					}
+					// tt = z.Next() // Text, most likely '\n'.
+					// tt = z.Next() // Start tag, <li>
+					// tt = z.Next() // Start tag <a>
+					// //tt = z.Next() // Is <a>THIS</a>
+					// token = z.Token()
+
+					// if token.Data == "a" {
+					// 	fmt.Println(token.Data)
+					// }
+					// ta := token.Attr
+					// fmt.Println(ta)
+					// return
+
+					// Get the link first.
+					link := token.Attr
+					// Now move onto get the name of the work, <a>TITLE</a>.
+					tt = z.Next()
+					token = z.Token()
+					title := token.Data
+					// Add the title with it's corresponding link to the map.
+					if len(link) > 0 {
+						if link[0].Key == "href" {
+							retval[title] = link[0].Val
+						}
+					}
 				}
-				return
+
 			}
 		}
 	}
@@ -55,5 +79,10 @@ func main() {
 	body := fetchHTML("https://www.aozora.gr.jp/index_pages/person20.html")
 	defer body.Close()
 
-	tokenize(body)
+	mm := tokenize(body)
+	i := 1
+	for key, val := range mm {
+		fmt.Println(i, key, val)
+		i++
+	}
 }
