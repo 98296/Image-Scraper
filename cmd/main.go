8ecef3e10c709fa517214672c84ac4152c24851d
travis <travis.hend@gmail.com>
2021-01-30 11:47:34 -0800

    Now have cl flags

--- cmd/main.go
+++ cmd/main.go
@@ -2,22 +2,25 @@ package main
 
 import (
 	"AozoraScraper/scraper"
+	"flag"
 	"log"
 	"os"
 )
 
 func main() {
+	ap := flag.String("ap", "https://www.aozora.gr.jp/index_pages/person20.html", "The url to the author's page")
+	dn := flag.String("dn", "works", "The directory you want to save the author's work, too. Must be a new folder")
+	flag.Parse()
+
 	// Go to author page and get the HTML response.
-	ap := "https://www.aozora.gr.jp/index_pages/person20.html"
-	body := scraper.FetchHTML(ap)
+	body := scraper.FetchHTML(*ap)
 	defer body.Close()
 
 	// Tokenize the author page into a map of URLs.
 	mm := scraper.ParseAP(body)
 
 	// Create a directory (directory name) to save the work to.
-	dn := "works/"
-	err := os.Mkdir(dn, 0755)
+	err := os.Mkdir(*dn, 0755)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -28,7 +31,7 @@ func main() {
 		body = scraper.FetchHTML(val)
 		// Then on that web page, find the link to the zip of the work.
 		zl := scraper.GetZipLink(body, val)
-		fn := dn + key + ".zip"
+		fn := *dn + "/" + key + ".zip"
 		scraper.DownloadFile(fn, zl)
 		if i == 1 {
 			break
